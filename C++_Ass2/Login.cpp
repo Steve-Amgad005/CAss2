6bfe2c578f4293dd60665c839d9df870f80e03f2
Steve Amgad <steve.amgad.005@gmail.com>
2025-12-14 23:56:01 +0200

    Add project files.

---
+++ C++_Ass2/Login.cpp
@@ -0,0 +1,9 @@
+#include "Login.h"
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew CAss2::Login);
+}
