645e6b52b7016274f0a1f8fcd870a2773bd6bd30
Salem Ramadan <salemramdan12345@gmail.com>
2025-12-15 00:27:35 +0200

    Forgot Update

--- C++_Ass2/Forgot1.h
+++ C++_Ass2/Forgot1.h
@@ -34,6 +34,17 @@ namespace CAss2 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Panel^ panel1;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+
+	private: System::Windows::Forms::TextBox^ textBox2;
+
+	private: System::Windows::Forms::Label^ label3;
+
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox1;
 
 	private:
 		/// <summary>
@@ -48,12 +59,116 @@ namespace CAss2 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Forgot";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Forgot::typeid));
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::Purple;
+			this->panel1->Controls->Add(this->textBox1);
+			this->panel1->Controls->Add(this->label2);
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Controls->Add(this->textBox2);
+			this->panel1->Controls->Add(this->label3);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Location = System::Drawing::Point(124, 63);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(624, 366);
+			this->panel1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::Purple;
+			this->button1->Location = System::Drawing::Point(199, 305);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(226, 48);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Back";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(302, 130);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(248, 33);
+			this->textBox2->TabIndex = 4;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(40, 130);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(234, 33);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"National Number";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(206, 33);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(219, 39);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Forgot Code";
+			this->label1->Click += gcnew System::EventHandler(this, &Forgot::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(222, 185);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(178, 33);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"Your code is";
+			this->label2->Click += gcnew System::EventHandler(this, &Forgot::label2_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(187, 230);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(248, 33);
+			this->textBox1->TabIndex = 8;
+			// 
+			// Forgot
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(872, 493);
+			this->Controls->Add(this->panel1);
+			this->Name = L"Forgot";
+			this->Text = L"Forgot";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
