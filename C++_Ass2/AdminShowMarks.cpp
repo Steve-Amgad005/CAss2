2bf9e639982a79f1a29c46eff3c45518def2694d
zzeyadnader <zeyadnader00@gmail.com>
2025-12-15 20:35:02 +0200

    Add half of Admin Marks show frame , (zh2t).

---
+++ C++_Ass2/AdminShowMarks.cpp
@@ -0,0 +1,2 @@
+#include "AdminShowMarks.h"
+
---
+++ C++_Ass2/AdminShowMarks.h
@@ -0,0 +1,209 @@
+#pragma once
+
+namespace CAss2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for AdminShowMarks
+	/// </summary>
+	public ref class AdminShowMarks : public System::Windows::Forms::Form
+	{
+	public:
+		AdminShowMarks(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~AdminShowMarks()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ StudentCode;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ StudentName;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Course;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Ass1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Ass2;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ CW;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Final;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Total;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::ComboBox^ comboBox2;
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+
+
+
+
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle10 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle11 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle12 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->StudentCode = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->StudentName = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Course = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Ass1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Ass2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->CW = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Final = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Total = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			dataGridViewCellStyle10->BackColor = System::Drawing::Color::White;
+			this->dataGridView1->AlternatingRowsDefaultCellStyle = dataGridViewCellStyle10;
+			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
+			dataGridViewCellStyle11->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle11->BackColor = System::Drawing::Color::DarkGray;
+			dataGridViewCellStyle11->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			dataGridViewCellStyle11->ForeColor = System::Drawing::SystemColors::WindowText;
+			dataGridViewCellStyle11->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle11->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle11->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->dataGridView1->ColumnHeadersDefaultCellStyle = dataGridViewCellStyle11;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(8) {
+				this->StudentCode,
+					this->StudentName, this->Course, this->Ass1, this->Ass2, this->CW, this->Final, this->Total
+			});
+			this->dataGridView1->Location = System::Drawing::Point(147, 235);
+			this->dataGridView1->Name = L"dataGridView1";
+			dataGridViewCellStyle12->BackColor = System::Drawing::Color::White;
+			this->dataGridView1->RowsDefaultCellStyle = dataGridViewCellStyle12;
+			this->dataGridView1->Size = System::Drawing::Size(750, 292);
+			this->dataGridView1->TabIndex = 11;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &AdminShowMarks::dataGridView1_CellContentClick);
+			// 
+			// StudentCode
+			// 
+			this->StudentCode->HeaderText = L"Student Code";
+			this->StudentCode->Name = L"StudentCode";
+			// 
+			// StudentName
+			// 
+			this->StudentName->HeaderText = L"Sudent Name";
+			this->StudentName->Name = L"StudentName";
+			// 
+			// Course
+			// 
+			this->Course->HeaderText = L"Course";
+			this->Course->Name = L"Course";
+			this->Course->ReadOnly = true;
+			// 
+			// Ass1
+			// 
+			this->Ass1->HeaderText = L"Assignment 1";
+			this->Ass1->Name = L"Ass1";
+			// 
+			// Ass2
+			// 
+			this->Ass2->HeaderText = L"Assignment 2";
+			this->Ass2->Name = L"Ass2";
+			// 
+			// CW
+			// 
+			this->CW->HeaderText = L"CW";
+			this->CW->Name = L"CW";
+			// 
+			// Final
+			// 
+			this->Final->HeaderText = L"Final";
+			this->Final->Name = L"Final";
+			// 
+			// Total
+			// 
+			this->Total->HeaderText = L"Total";
+			this->Total->Name = L"Total";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(587, 119);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(123, 21);
+			this->comboBox1->TabIndex = 12;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Location = System::Drawing::Point(749, 119);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(123, 21);
+			this->comboBox2->TabIndex = 13;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(378, 27);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(262, 39);
+			this->label1->TabIndex = 14;
+			this->label1->Text = L"Show Students";
+			// 
+			// AdminShowMarks
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Purple;
+			this->ClientSize = System::Drawing::Size(1002, 539);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->dataGridView1);
+			this->Name = L"AdminShowMarks";
+			this->Text = L"AdminShowMarks";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	}
+};
+}
