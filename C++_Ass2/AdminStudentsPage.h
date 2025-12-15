d46b0c52162caecfbde8b0e89628f70fb0a14a67
Steve Amgad <steve.amgad.005@gmail.com>
2025-12-15 02:29:29 +0200

    Admin Student Page

---
+++ C++_Ass2/AdminStudentsPage.h
@@ -0,0 +1,302 @@
+#pragma once
+
+namespace CAss2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for AdminStudentsPage
+	/// </summary>
+	public ref class AdminStudentsPage : public System::Windows::Forms::Form
+	{
+	public:
+		AdminStudentsPage(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~AdminStudentsPage()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Panel^ panel1;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ StudentName;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ StudentCode;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ NID;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Department;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Year;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Term;
+	private: System::Windows::Forms::DataGridViewImageColumn^ Image;
+	private: System::Windows::Forms::DataGridViewCheckBoxColumn^ Fees;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(AdminStudentsPage::typeid));
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle4 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle5 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle6 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->StudentName = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->StudentCode = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->NID = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Department = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Year = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Term = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Image = (gcnew System::Windows::Forms::DataGridViewImageColumn());
+			this->Fees = (gcnew System::Windows::Forms::DataGridViewCheckBoxColumn());
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Location = System::Drawing::Point(54, 12);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(1063, 100);
+			this->panel1->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(301, 26);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(599, 39);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"New Cairo Technological University";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(27, 7);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(110, 84);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->button1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.BackgroundImage")));
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Location = System::Drawing::Point(1138, 33);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(71, 65);
+			this->button1->TabIndex = 2;
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->button2->FlatAppearance->BorderSize = 0;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::Color::White;
+			this->button2->Location = System::Drawing::Point(911, 154);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(187, 60);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"Refresh";
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::Color::White;
+			this->button3->Location = System::Drawing::Point(644, 154);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(187, 60);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"Delete Student";
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->button4->FlatAppearance->BorderSize = 0;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button4->ForeColor = System::Drawing::Color::White;
+			this->button4->Location = System::Drawing::Point(362, 154);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(187, 60);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"Edit Student";
+			this->button4->UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this->button5->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->button5->FlatAppearance->BorderSize = 0;
+			this->button5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->ForeColor = System::Drawing::Color::White;
+			this->button5->Location = System::Drawing::Point(81, 154);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(187, 60);
+			this->button5->TabIndex = 3;
+			this->button5->Text = L"Add Student";
+			this->button5->UseVisualStyleBackColor = false;
+			this->button5->Click += gcnew System::EventHandler(this, &AdminStudentsPage::button5_Click);
+			// 
+			// dataGridView1
+			// 
+			dataGridViewCellStyle4->BackColor = System::Drawing::Color::White;
+			this->dataGridView1->AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+			dataGridViewCellStyle5->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle5->BackColor = System::Drawing::Color::DarkGray;
+			dataGridViewCellStyle5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular,
+				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
+			dataGridViewCellStyle5->ForeColor = System::Drawing::SystemColors::WindowText;
+			dataGridViewCellStyle5->SelectionBackColor = System::Drawing::SystemColors::Highlight;
+			dataGridViewCellStyle5->SelectionForeColor = System::Drawing::SystemColors::HighlightText;
+			dataGridViewCellStyle5->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->dataGridView1->ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(8) {
+				this->StudentName,
+					this->StudentCode, this->NID, this->Department, this->Year, this->Term, this->Image, this->Fees
+			});
+			this->dataGridView1->Location = System::Drawing::Point(189, 255);
+			this->dataGridView1->Name = L"dataGridView1";
+			dataGridViewCellStyle6->BackColor = System::Drawing::Color::White;
+			this->dataGridView1->RowsDefaultCellStyle = dataGridViewCellStyle6;
+			this->dataGridView1->Size = System::Drawing::Size(838, 302);
+			this->dataGridView1->TabIndex = 4;
+			// 
+			// StudentName
+			// 
+			this->StudentName->HeaderText = L"Student Name";
+			this->StudentName->Name = L"StudentName";
+			// 
+			// StudentCode
+			// 
+			this->StudentCode->HeaderText = L"Student Code";
+			this->StudentCode->Name = L"StudentCode";
+			// 
+			// NID
+			// 
+			this->NID->HeaderText = L"NID";
+			this->NID->Name = L"NID";
+			// 
+			// Department
+			// 
+			this->Department->HeaderText = L"Department";
+			this->Department->Name = L"Department";
+			// 
+			// Year
+			// 
+			this->Year->HeaderText = L"Year";
+			this->Year->Name = L"Year";
+			// 
+			// Term
+			// 
+			this->Term->HeaderText = L"Term";
+			this->Term->Name = L"Term";
+			// 
+			// Image
+			// 
+			this->Image->HeaderText = L"Image";
+			this->Image->Name = L"Image";
+			// 
+			// Fees
+			// 
+			this->Fees->HeaderText = L"Fees";
+			this->Fees->Name = L"Fees";
+			// 
+			// AdminStudentsPage
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Purple;
+			this->ClientSize = System::Drawing::Size(1242, 599);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->panel1);
+			this->Name = L"AdminStudentsPage";
+			this->Text = L"AdminStudentsPage";
+			this->Load += gcnew System::EventHandler(this, &AdminStudentsPage::AdminStudentsPage_Load);
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void AdminStudentsPage_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
